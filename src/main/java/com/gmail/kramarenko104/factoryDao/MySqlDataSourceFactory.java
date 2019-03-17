17065f1b86e89b5aca484235c0e9f65989318b70
julia_kramarenko <kramarenko104@gmail.com>
2019-03-17 21:29:20 +0200

    DaoFactory refactoring

--- src/main/java/com/gmail/kramarenko104/factoryDao/MySqlDataSourceFactory.java
+++ src/main/java/com/gmail/kramarenko104/factoryDao/MySqlDataSourceFactory.java
@@ -1,6 +1,5 @@
 package com.gmail.kramarenko104.factoryDao;
 
-import com.gmail.kramarenko104.dao.*;
 import org.apache.log4j.Logger;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
@@ -11,16 +10,10 @@ import org.apache.commons.dbcp2.BasicDataSource;
 public class MySqlDataSourceFactory extends DaoFactory {
 
     private static Logger logger = Logger.getLogger(MySqlDataSourceFactory.class);
-    private UserDaoMySqlImpl userDaoMySqlImpl;
-    private ProductDaoMySqlImpl productDaoMySqlImpl;
-    private CartDaoMySqlImpl cartDaoMySqlImpl;
-    private OrderDaoMySqlImpl orderDaoMySqlImpl;
     private BasicDataSource dataSource;
-    private Connection conn;
 
-    public MySqlDataSourceFactory(){
-        if (dataSource == null)
-        {
+    public MySqlDataSourceFactory() {
+        if (dataSource == null) {
             ResourceBundle config = null;
             try {
                 config = ResourceBundle.getBundle("dbconfig");
@@ -42,7 +35,8 @@ public class MySqlDataSourceFactory extends DaoFactory {
     @Override
     public void openConnection() {
         try {
-            conn = dataSource.getConnection();
+            Connection conn = dataSource.getConnection();
+            super.setConnection(conn);
             logger.debug("Connection obtained...");
         } catch (SQLException e) {
             logger.debug("Connection failed. SQLException: " + e.getMessage());
@@ -51,64 +45,7 @@ public class MySqlDataSourceFactory extends DaoFactory {
 
     @Override
     public void closeConnection() {
-        try {
-            if (conn != null)
-                conn.close();
-            logger.debug("Connection closed...");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public UserDao getUserDao() {
-        userDaoMySqlImpl = new UserDaoMySqlImpl(conn);
-        return userDaoMySqlImpl;
-    }
-
-    @Override
-    public ProductDao getProductDao() {
-        productDaoMySqlImpl = new ProductDaoMySqlImpl(conn);
-        return productDaoMySqlImpl;
-    }
-
-    @Override
-    public CartDao getCartDao() {
-        cartDaoMySqlImpl = new CartDaoMySqlImpl(conn);
-        return cartDaoMySqlImpl;
-    }
-
-    @Override
-    public OrderDao getOrderDao() {
-        orderDaoMySqlImpl = new OrderDaoMySqlImpl(conn);
-        return orderDaoMySqlImpl;
-    }
-
-    @Override
-    public void deleteUserDao(UserDao userDao) {
-        if (userDao != null) {
-            userDao = null;
-        }
-    }
-
-    @Override
-    public void deleteProductDao(ProductDao productDao) {
-        if (productDao != null) {
-            productDao = null;
-        }
-    }
-
-    @Override
-    public void deleteCartDao(CartDao cartDao) {
-        if (cartDao != null) {
-            cartDao = null;
-        }
-    }
-
-    @Override
-    public void deleteOrderDao(OrderDao orderDao) {
-        if (orderDao != null) {
-            orderDao = null;
-        }
+        super.closeConnection();
+        logger.debug("Connection closed...");
     }
 }
--- src/main/java/com/gmail/kramarenko104/factoryDao/MySqlJDBCDaoFactory.java
+++ src/main/java/com/gmail/kramarenko104/factoryDao/MySqlJDBCDaoFactory.java
@@ -1,6 +1,5 @@
 package com.gmail.kramarenko104.factoryDao;
 
-import com.gmail.kramarenko104.dao.*;
 import org.apache.log4j.Logger;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -11,12 +10,7 @@ import java.util.ResourceBundle;
 public class MySqlJDBCDaoFactory extends DaoFactory {
 
     private static Logger logger = Logger.getLogger(MySqlJDBCDaoFactory.class);
-    private UserDaoMySqlImpl userDaoMySqlImpl;
-    private ProductDaoMySqlImpl productDaoMySqlImpl;
-    private CartDaoMySqlImpl cartDaoMySqlImpl;
-    private OrderDaoMySqlImpl orderDaoMySqlImpl;
     private String connStr;
-    private Connection conn;
 
     public MySqlJDBCDaoFactory() {
         ResourceBundle config = null;
@@ -42,8 +36,9 @@ public class MySqlJDBCDaoFactory extends DaoFactory {
     @Override
     public void openConnection() {
         try {
-            conn = DriverManager.getConnection(connStr);
-            logger.debug("Connection obtained: " + conn);
+            Connection conn = DriverManager.getConnection(connStr);
+            super.setConnection(conn);
+            logger.debug("Connection obtained");
         } catch (SQLException e) {
             logger.debug("Connection failed. SQLException: " + e.getMessage());
         }
@@ -51,64 +46,7 @@ public class MySqlJDBCDaoFactory extends DaoFactory {
 
     @Override
     public void closeConnection() {
-        try {
-            if (conn != null)
-                conn.close();
-            logger.debug("Connection to db is closed");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public UserDao getUserDao() {
-        userDaoMySqlImpl = new UserDaoMySqlImpl(conn);
-        return userDaoMySqlImpl;
-    }
-
-    @Override
-    public ProductDao getProductDao() {
-        productDaoMySqlImpl = new ProductDaoMySqlImpl(conn);
-        return productDaoMySqlImpl;
-    }
-
-    @Override
-    public CartDao getCartDao() {
-        cartDaoMySqlImpl = new CartDaoMySqlImpl(conn);
-        return cartDaoMySqlImpl;
-    }
-
-    @Override
-    public OrderDao getOrderDao() {
-        orderDaoMySqlImpl = new OrderDaoMySqlImpl(conn);
-        return orderDaoMySqlImpl;
-    }
-
-    @Override
-    public void deleteUserDao(UserDao userDao) {
-        if (userDao != null) {
-            userDao = null;
-        }
-    }
-
-    @Override
-    public void deleteProductDao(ProductDao productDao) {
-        if (productDao != null) {
-            productDao = null;
-        }
-    }
-
-    @Override
-    public void deleteCartDao(CartDao cartDao) {
-        if (cartDao != null) {
-            cartDao = null;
-        }
-    }
-
-    @Override
-    public void deleteOrderDao(OrderDao orderDao) {
-        if (orderDao != null) {
-            orderDao = null;
-        }
+        super.closeConnection();
+        logger.debug("Connection to db is closed");
     }
 }
